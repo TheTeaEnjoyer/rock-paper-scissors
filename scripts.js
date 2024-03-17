064b91c4829abf3acfda0ebffa0b8bdd88732f28
TheTeaEnjoyer <124472583+TheTeaEnjoyer@users.noreply.github.com>
2024-03-17 06:58:20 -0400

    Add playGame function

--- scripts.js
+++ scripts.js
@@ -18,26 +18,68 @@ function getComputerChoice() {
 //Function that declares the winner
 function playRound(playerSelection, computerSelection) {
 
-    //Making both selections lower case for easier comparisons (case insensitive)
-    let playerSelectionToLowerCase = playerSelection.toLowerCase();
-    let computerSelectionToLowerCase = computerSelection.toLowerCase();
+    //Making both selections upper case for easier comparisons (case insensitive)
+    let playerSelectionToUpperCase = playerSelection.toUpperCase();
+    let computerSelectionToUpperCase = computerSelection.toUpperCase();
 
     //Player draw Scenario
-    if(playerSelectionToLowerCase == computerSelectionToLowerCase) {
-        return `Draw! You both chose ${playerSelection}`;
+    if(playerSelectionToUpperCase == computerSelectionToUpperCase) {
+        return `Draw! You both chose ${playerSelectionToUpperCase}`;
     }
     //Player loss scenarios
-    else if(playerSelectionToLowerCase == 'rock' && computerSelectionToLowerCase == 'paper') {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    else if(playerSelectionToUpperCase == 'ROCK' && computerSelectionToUpperCase == 'PAPER') {
+        return `You Lose! ${computerSelectionToUpperCase} beats ${playerSelectionToUpperCase}`;
     }
-    else if(playerSelectionToLowerCase == 'paper' && computerSelectionToLowerCase == 'scissors') {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    else if(playerSelectionToUpperCase == 'PAPER' && computerSelectionToUpperCase == 'SCISSORS') {
+        return `You Lose! ${computerSelectionToUpperCase} beats ${playerSelectionToUpperCase}`;
     }
-    else if(playerSelectionToLowerCase == 'scissors' && computerSelectionToLowerCase == 'rock') {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    else if(playerSelectionToUpperCase == 'SCISSORS' && computerSelectionToUpperCase == 'ROCK') {
+        return `You Lose! ${computerSelectionToUpperCase} beats ${playerSelectionToUpperCase}`;
     }
     //Player win scenarios
     else {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
+        return `You Win! ${playerSelectionToUpperCase} beats ${computerSelectionToUpperCase}`;
+    }
+}
+
+//Function that plays five rounds of Rock Paper Scissors, while keeping score and announcing the winner at the end
+function playGame() {
+    //Initializes scores
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for(let i = 0; i < 5; i++) {
+
+        //Stores player choice in playerSelection variable
+        let playerSelection = prompt('Rock, Paper or Scissors?');
+        let computerSelection = getComputerChoice();
+        //Saves the result of the round in the roundResult variable
+        let roundResult = playRound(playerSelection, computerSelection);
+
+        //Displays the result of the round
+        console.log(roundResult);
+
+        //Adds points to the winner and displays the points
+        if(roundResult.includes('Win')) {
+            playerScore++;
+            console.log(`Player: ${playerScore} --- Computer: ${computerScore}`);
+        }
+        else if(roundResult.includes('Lose')) {
+            computerScore++;
+            console.log(`Player: ${playerScore} --- Computer: ${computerScore}`);
+        }
+        else {
+            console.log(`Player: ${playerScore} --- Computer: ${computerScore}`);
+        }
+    }
+
+    if(playerScore > computerScore) {
+        console.log('Congrats you won!')
+    }
+    else if (playerScore < computerScore) {
+        console.log('You lost!');
+    }
+    else {
+        console.log('You drew!')
     }
 }
